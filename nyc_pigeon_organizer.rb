a298c8a77c08d20821bd0c7399ca3641984d2b00
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-09 15:42:38 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,17 +6,29 @@ def nyc_pigeon_organizer(data)
       array.each do |name|
         if !new_hash.has_key?(name)
           new_hash[name] = {}
-        end
-
+        end 
         if !new_hash[name].has_key?(property)
           new_hash[name][property] = []
-        end
-
+        end 
         if !new_hash[name][property].include?(attribute)
           new_hash[name][property] << attribute.to_s
         end
-      end
-    end
-  end
-  new_hash
+      end 
+    end 
+  end 
+new_hash
 end
+puts nyc_pigeon_organizer( {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+})
